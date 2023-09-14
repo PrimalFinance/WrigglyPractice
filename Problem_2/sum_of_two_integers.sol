8b8e05153ee69bb271d068312e3bfb2fdd765749
PrimalFinance <primalfinance101@gmail.com>
2023-09-14 11:22:33 -0700

    Updated with problem 2.

---
+++ Problem_2/sum_of_two_integers.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Solution {
+    /**
+     * @param {uint256} a
+     * @param {uint256} b
+     * @return uint256
+     */
+    function sumOfTwoIntegers(
+        uint256 a,
+        uint256 b
+    ) public pure returns (uint256) {
+        //TODO
+        // Create an unsigned variable named "sum" to hold the sum of the two parameters.
+        uint256 sum = a + b;
+        return sum;
+    }
+}
