1acb3be012d9f98cc205ced92f7b65970093adda
PrimalFinance <primalfinance101@gmail.com>
2023-09-14 11:16:31 -0700

    Test commit.

---
+++ Problem_1/hello_world.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Solution {
+    /**
+     * @return string
+     */
+    function helloWorld() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
