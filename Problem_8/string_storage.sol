58594e30ef2a5505f6a1049faf577d689700587e
PrimalFinance <primalfinance101@gmail.com>
2023-09-14 12:23:10 -0700

    Added more notes.

--- Problem_8/string_storage.sol
+++ Problem_8/string_storage.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.0;
 contract StringStorage {
     string private storedString;
 
+    // NOTE: The memory keyword is used in the parameter. This means the data passed in the parameter is stored in "memory" and not on the blockchain.
+    // This has a few advantages, the largest being it is *more gas efficient*.
     function set(string memory value) public {
         // TODO
         // Set the value of the "storedString" to the parameter value.
