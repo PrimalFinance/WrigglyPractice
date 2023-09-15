d47e4be479e08fdd3ee177de25b163e65f7874ff
PrimalFinance <primalfinance101@gmail.com>
2023-09-15 11:59:18 -0700

    Updated with Problem 36.

---
+++ Problem_36/simple_hash_two_RAW.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Solution {
+    function ripemdHash(string memory _message) public pure returns (bytes20) {
+        // TODO
+    }
+}
